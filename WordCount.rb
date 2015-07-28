1829f37509438b396401c4426c95e8e8b91faebc
yeinee <dls0931@gmail.com>
2015-07-28 18:43:59 +0900

    Create WordCount.rb

---
+++ WordCount.rb
@@ -0,0 +1,11 @@
+def WordCount(str)
+
+  # code goes here
+  str = str.split.length
+  return str 
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+WordCount(STDIN.gets)           
