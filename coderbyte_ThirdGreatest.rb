75bd5623fc33d0540f8077401cd34050b48ebc64
yeinee <dls0931@gmail.com>
2015-07-30 09:35:44 +0900

    Update coderbyte_ThirdGreatest.rb

--- coderbyte_ThirdGreatest.rb
+++ coderbyte_ThirdGreatest.rb
@@ -7,33 +7,12 @@ def ThirdGreatest(strArr)
     hash[x] = x.length
   end
   
-  a = hash.sort.reverse[2][1]
+  a = hash.sort_by {|k,v| v }.reverse[2][1]
   
   return hash.select {|k,v| v==a}[0][0]
-  
-  #return a
          
 end
    
 # keep this function call here 
 # to see how to enter arguments in Ruby scroll down   
-ThirdGreatest(STDIN.gets)  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
+ThirdGreatest(STDIN.gets)           
