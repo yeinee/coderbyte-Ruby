47b31bc1030680a1b77cd5e1aac8bf408ac4896b
yeinee <dls0931@gmail.com>
2015-07-30 14:22:50 +0900

    Update coderbyte_DivisionStringified.rb

--- coderbyte_DivisionStringified.rb
+++ coderbyte_DivisionStringified.rb
@@ -1,7 +1,11 @@
 def DivisionStringified(num1,num2)
 
   # code goes here
-  devide = (num1 / num2).ceil
+  if num1 < num2
+    devide = 1
+  else
+    devide = (num1 / num2).ceil
+  end
   
   return devide.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
          
@@ -9,23 +13,4 @@ end
    
 # keep this function call here 
 # to see how to enter arguments in Ruby scroll down   
-DivisionStringified(STDIN.gets)  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
+DivisionStringified(STDIN.gets)           
